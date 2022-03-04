55ff4516581268851ec9ee34ae4f76cdc17a441e
csemahmud <cse.mahmudul@gmail.com>
2022-03-04 18:03:33 +0900

    Finished Customer_Model

--- application/models/customer_model.php
+++ application/models/customer_model.php
@@ -20,4 +20,30 @@ class Customer_Model extends CI_Model {
         $this->db->where("customer_email", $customer_email);
         return $this->db->get()->result();
     }
+    
+    public function insert_customer_info($data) {
+        $value_return = $this->db->insert("tbl_customer", $data);
+        if($value_return > 0){
+            $sdata = array();
+            $sdata["customer_id"] = $this->db->insert_id();
+            $sdata["customer_name"] = $data["first_name"]." ".$data["last_name"];
+            $this->session->set_userdata($sdata);
+        }
+        return $value_return;
+    }
+    
+    public function select_customer_by_email_password($customer_email, $customer_password) {
+        $this->db->select("*");
+        $this->db->from("tbl_customer");
+        $this->db->where("customer_email", $customer_email);
+        $this->db->where("customer_password", $customer_password);
+        return $this->db->get()->row();
+    }
+    
+    public function select_customer_by_id($customer_id) {
+        $this->db->select("*");
+        $this->db->from("tbl_customer");
+        $this->db->where("customer_id", $customer_id);
+        return $this->db->get()->row();
+    }
 }
