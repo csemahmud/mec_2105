cbb0230f28b776635fecec5e3722a962205fe353
csemahmud <cse.mahmudul@gmail.com>
2022-03-15 17:25:18 +0900

    Added Payment_Controller and Shipping_Controller from office ubuntu

---
+++ application/controllers/payment_controller.php
@@ -0,0 +1,39 @@
+<?php if(! defined('BASEPATH')) exit('No Direct Script access Allowed');
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Payment_Controller
+ *
+ * @author Mahmudul Hasan Khan CSE
+ */
+class Payment_Controller extends CI_Controller {
+    //put your code here
+    
+    public function index(){
+            
+            $mdata = array();
+            $mdata["all_published_categories"] = $this->front_model->select_categories_by_publication_status(1);
+            $mdata["all_published_manufacturers"] = $this->front_model->select_manufacturers_by_publication_status(1);
+            $cdata = array();
+            $customer_id = $this->session->userdata("customer_id");
+            $shipping_id = $this->session->userdata("shipping_id");
+            if(($customer_id != NULL) && ($shipping_id != NULL)){
+                $pdata = array();
+                $pdata["all_payments"] = $this->payment_model->select_all_payments();
+                $cdata["checkout_component"] = $this->load->view("payment/payment_method_component", $pdata, TRUE);
+            } elseif ($shipping_id == null) {
+                redirect("cart_controller");
+            }
+            $data = array();
+            $data["submenu"] = $this->load->view("front/home_submenu_component", $mdata, TRUE);
+            $data["main_content"] = $this->load->view("cart/cart_component", $cdata, TRUE);
+            $data["title"] = "Payment";
+            $this->load->view("shared/front_naster_ui", $data);
+            
+    }
+}
---
+++ application/controllers/shipping_controller.php
@@ -0,0 +1,73 @@
+<?php if(! defined('BASEPATH')) exit('No Direct Script access Allowed');
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Shipping_Controller
+ *
+ * @author Mahmudul Hasan Khan CSE
+ */
+class Shipping_Controller extends CI_Controller {
+    //put your code here
+    
+    public function save_shipping_info() {
+
+        $data = array();
+        $data["s_first_name"] = $this->input->post("s_first_name", TRUE);
+        $data["s_last_name"] = $this->input->post("s_last_name", TRUE);
+        $data["s_email"] = $this->input->post("s_email", TRUE);
+        $data["s_mobile"] = $this->input->post("s_mobile", TRUE);
+        $data["s_phone"] = $this->input->post("s_phone", TRUE);
+        $data["s_fax"] = $this->input->post("s_fax", TRUE);
+        $data["s_company"] = $this->input->post("s_company", TRUE);
+        $data["s_address"] = $this->input->post("s_address", TRUE);
+        $data["s_city"] = $this->input->post("s_city", TRUE);
+        $data["s_country"] = $this->input->post("s_country", TRUE);
+        $data["s_zip_code"] = $this->input->post("s_zip_code", TRUE);
+        $sdata = array();
+        if($this->shipping_model->insert_shipping_info($data) > 0){
+            $sdata["alert"] = "SAVED Shipping Information Successfully .";
+        } else {
+            $sdata["alert"] = "Could NOT Save Shipping Information";
+        }
+        $this->session->set_userdata($sdata);
+        redirect("payment_controller");
+        
+    }
+    
+    public function shipping_same_as_billing() {
+
+        $sdata = array();
+        $customer_id = $this->session->userdata("customer_id");
+        if($customer_id != NULL) {
+            $data = array();
+            $data["s_first_name"] = $this->input->post("s_first_name", TRUE);
+            $data["s_last_name"] = $this->input->post("s_last_name", TRUE);
+            $data["s_email"] = $this->input->post("s_email", TRUE);
+            $data["s_mobile"] = $this->input->post("s_mobile", TRUE);
+            $data["s_phone"] = $this->input->post("s_phone", TRUE);
+            $data["s_fax"] = $this->input->post("s_fax", TRUE);
+            $data["s_company"] = $this->input->post("s_company", TRUE);
+            $data["s_address"] = $this->input->post("s_address", TRUE);
+            $data["s_city"] = $this->input->post("s_city", TRUE);
+            $data["s_country"] = $this->input->post("s_country", TRUE);
+            $data["s_zip_code"] = $this->input->post("s_zip_code", TRUE);
+            $sdata = array();
+            if($this->shipping_model->insert_shipping_info($data) > 0){
+                $sdata["alert"] = "SAVED Shipping Information Successfully .";
+            } else {
+                $sdata["alert"] = "Could NOT Save Shipping Information";
+            }
+        } else {
+            $sdata["alert"] = "No Customer is Logged in.";
+        }
+        $this->session->set_userdata($sdata);
+        redirect("payment_controller");
+        
+    }
+
+}
